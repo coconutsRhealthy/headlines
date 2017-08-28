7caa6d190e80fecc6da28ed77b3b25591b3dfd9e
LennartMac <lennartpopma@hotmail.com>
2017-08-28 20:25:59 +0200

    Addes sites to BigDbStorerEntertainment

--- src/main/java/com/lennart/model/headlinesBigDb/headlinesBigDbEntertainment/BigDbStorerEntertainment.java
+++ src/main/java/com/lennart/model/headlinesBigDb/headlinesBigDbEntertainment/BigDbStorerEntertainment.java
@@ -152,181 +152,181 @@ public class BigDbStorerEntertainment extends BigDbStorer {
     public void initializeDocuments(int number) throws IOException {
         switch(number) {
             case 1:
-                document1 = readSite("...");
+                document1 = readSite("https://www.yahoo.com/celebrity");
                 break;
             case 2:
-                document2 = readSite("...");
+                document2 = readSite("http://www.tmz.com");
                 break;
             case 3:
-                document3 = readSite("...");
+                document3 = readSite("http://www.eonline.com");
                 break;
             case 4:
-                document4 = readSite("...");
+                document4 = readSite("http://people.com");
                 break;
             case 5:
-                document5 = readSite("...");
+                document5 = readSite("http://www.usmagazine.com");
                 break;
             case 6:
-                document6 = readSite("...");
+                document6 = readSite("http://www.wonderwall.com");
                 break;
             case 7:
-                document7 = readSite("...");
+                document7 = readSite("http://www.zimbio.com");
                 break;
             case 8:
-                document8 = readSite("...");
+                document8 = readSite("http://perezhilton.com");
                 break;
             case 9:
-                document9 = readSite("...");
+                document9 = readSite("http://hollywoodlife.com");
                 break;
             case 10:
-                document10 = readSite("...");
+                document10 = readSite("http://radaronline.com");
                 break;
             case 11:
-                document11 = readSite("...");
+                document11 = readSite("https://www.popsugar.com");
                 break;
             case 12:
-                document12 = readSite("...");
+                document12 = readSite("http://www.wetpaint.com");
                 break;
             case 13:
-                document13 = readSite("...");
+                document13 = readSite("http://mediatakeout.com");
                 break;
             case 14:
-                document14 = readSite("...");
+                document14 = readSite("http://toofab.com");
                 break;
             case 15:
-                document15 = readSite("...");
+                document15 = readSite("http://ohnotheydidnt.livejournal.com");
                 break;
             case 16:
-                document16 = readSite("...");
+                document16 = readSite("http://dlisted.com");
                 break;
             case 17:
-                document17 = readSite("...");
+                document17 = readSite("http://www.thesuperficial.com");
                 break;
             case 18:
-                document18 = readSite("...");
+                document18 = readSite("https://www.gofugyourself.com");
                 break;
             case 19:
-                document19 = readSite("...");
+                document19 = readSite("http://www.celebitchy.com");
                 break;
             case 20:
-                document20 = readSite("...");
+                document20 = readSite("http://www.celebslam.com");
                 break;
             case 21:
-                document21 = readSite("...");
+                document21 = readSite("http://www.egotastic.com");
                 break;
             case 22:
-                document22 = readSite("...");
+                document22 = readSite("http://www.hellomagazine.com");
                 break;
             case 23:
-                document23 = readSite("...");
+                document23 = readSite("http://idly.craveonline.com");
                 break;
             case 24:
-                document24 = readSite("...");
+                document24 = readSite("http://www.infdaily.org");
                 break;
             case 25:
-                document25 = readSite("...");
+                document25 = readSite("http://www.justjared.com");
                 break;
             case 26:
-                document26 = readSite("...");
+                document26 = readSite("http://www.socialitelife.com");
                 break;
             case 27:
-                document27 = readSite("...");
+                document27 = readSite("https://www.wesmirch.com");
                 break;
             case 28:
-                document28 = readSite("...");
+                document28 = readSite("http://www.wwtdd.com");
                 break;
             case 29:
-                document29 = readSite("...");
+                document29 = readSite("http://x17online.com");
                 break;
             case 30:
-                document30 = readSite("...");
+                document30 = readSite("https://www.thehollywoodgossip.com");
                 break;
             case 31:
-                document31 = readSite("...");
+                document31 = readSite("http://www.celebritygossip.com");
                 break;
             case 32:
-                document32 = readSite("...");
+                document32 = readSite("http://www.digitalspy.com");
                 break;
             case 33:
-                document33 = readSite("...");
+                document33 = readSite("http://www.latimes.com/entertainment");
                 break;
             case 34:
-                document34 = readSite("...");
+                document34 = readSite("http://variety.com");
                 break;
             case 35:
-                document35 = readSite("...");
+                document35 = readSite("http://www.bet.com");
                 break;
             case 36:
-                document36 = readSite("...");
+                document36 = readSite("http://theshaderoom.com");
                 break;
             case 37:
-                document37 = readSite("...");
+                document37 = readSite("http://www.lifeandstylemag.com");
                 break;
             case 38:
-                document38 = readSite("...");
+                document38 = readSite("http://www.glamourmagazine.co.uk");
                 break;
             case 39:
-                document39 = readSite("...");
+                document39 = readSite("http://www.cambio.com");
                 break;
             case 40:
-                document40 = readSite("...");
+                document40 = readSite("https://www.vanityfair.com");
                 break;
             case 41:
-                document41 = readSite("...");
+                document41 = readSite("http://www.intouchweekly.com");
                 break;
             case 42:
-                document42 = readSite("...");
+                document42 = readSite("http://www.entertainmentdaily.co.uk");
                 break;
             case 43:
-                document43 = readSite("...");
+                document43 = readSite("http://www.accesshollywood.com");
                 break;
             case 44:
-                document44 = readSite("...");
+                document44 = readSite("http://www.celebuzz.com");
                 break;
             case 45:
-                document45 = readSite("...");
+                document45 = readSite("https://www.reddit.com/r/entertainment");
                 break;
             case 46:
-                document46 = readSite("...");
+                document46 = readSite("https://www.buzzfeed.com/celebrity");
                 break;
             case 47:
-                document47 = readSite("...");
+                document47 = readSite("http://deadline.com");
                 break;
             case 48:
-                document48 = readSite("...");
+                document48 = readSite("http://globalnews.ca/entertainment");
                 break;
             case 49:
-                document49 = readSite("...");
+                document49 = readSite("http://thefix.nine.com.au");
                 break;
             case 51:
-                document51 = readSite("...");
+                document51 = readSite("http://celebrityinsider.org");
                 break;
             case 52:
-                document52 = readSite("...");
+                document52 = readSite("http://stylecaster.com");
                 break;
             case 53:
-                document53 = readSite("...");
+                document53 = readSite("https://www.thesun.co.uk/tvandshowbiz");
                 break;
             case 54:
-                document54 = readSite("...");
+                document54 = readSite("https://bossip.com");
                 break;
             case 55:
-                document55 = readSite("...");
+                document55 = readSite("http://www.laineygossip.com");
                 break;
             case 56:
-                document56 = readSite("...");
+                document56 = readSite("http://www.ok.co.uk");
                 break;
             case 57:
-                document57 = readSite("...");
+                document57 = readSite("https://www.gossipcop.com");
                 break;
             case 58:
-                document58 = readSite("...");
+                document58 = readSite("http://www.fame10.com");
                 break;
             case 59:
-                document59 = readSite("...");
+                document59 = readSite("http://etcanada.com");
                 break;
             case 60:
-                document60 = readSite("...");
+                document60 = readSite("http://sandrarose.com");
                 break;
         }
     }
